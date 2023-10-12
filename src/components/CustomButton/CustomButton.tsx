77f730258c11a0464581cb865a09fdec74346719
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2023-10-11 21:28:11 -0300

    first commit

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,55 +1,67 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import React from "react";
 
 interface CustomInputProps {
-    onPress: () => void;
-    text: string;
-    type?: string;
-    bgColor?: string;
-    fgColor?: string;
-  }
-
-const CustomButton: React.FC<CustomInputProps> = ({onPress, text, type = "PRIMARY", bgColor, fgColor}) => {
-  return (
-    <Pressable onPress={onPress} 
-    style={[
-        styles.container, 
-        styles[`container_${type}`],
-        bgColor ? {backgroundColor:bgColor}:{},
-    ]}>
-      <Text 
-        style={[styles.text, styles[`text_${type}`], fgColor ? {color: fgColor} : {},]
-        }>
-        {text}</Text>
-    </Pressable>
-  )
+  onPress: () => void;
+  text: string;
+  type?: string;
+  bgColor?: string;
+  fgColor?: string;
 }
 
+const CustomButton: React.FC<CustomInputProps> = ({
+  onPress,
+  text,
+  type = "PRIMARY",
+  bgColor,
+  fgColor,
+}: any) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[
+        styles.container,
+        styles[`container_${type}`],
+        bgColor ? { backgroundColor: bgColor } : {},
+      ]}
+    >
+      <Text
+        style={[
+          styles.text,
+          styles[`text_${type}`],
+          fgColor ? { color: fgColor } : {},
+        ]}
+      >
+        {text}
+      </Text>
+    </Pressable>
+  );
+};
+
 const styles = StyleSheet.create({
-    container:{
-        width: '100%',
-        padding: 10,
-        marginVertical: 5,
+  container: {
+    width: "100%",
+    padding: 10,
+    marginVertical: 5,
 
-        alignItems: 'center',
-        borderRadius: 5,
+    alignItems: "center",
+    borderRadius: 5,
+  },
 
-    },
+  container_PRIMARY: {
+    backgroundColor: "#3B71F3",
+  },
 
-    container_PRIMARY:{
-        backgroundColor: '#3B71F3',
-    },
+  container_TERTIARY: {},
 
-    container_TERTIARY:{},
+  text: {
+    fontWeight: "bold",
+    color: "white",
+  },
 
-    text: {
-        fontWeight: 'bold',
-        color: 'white',
-    },
-
-    text_TERTIARY:{
-        color: 'gray',
-    },
+  text_TERTIARY: {
+    color: "gray",
+  },
 });
 
-export default CustomButton
+export default CustomButton;
