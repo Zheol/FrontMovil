68f9e2d7a32642e7408e12929b534946558dfb34
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2023-12-14 21:00:25 -0300

    update

---
+++ components/ModalTareas.tsx
@@ -0,0 +1,182 @@
+// UserProfileModal.tsx
+import React from "react";
+import { Text, View } from "react-native";
+import { Modal, Portal, Button, Divider } from "react-native-paper";
+import Font from "../constants/Font";
+import FontSize from "../constants/FontSize";
+import { useNavigation } from "@react-navigation/native";
+import AppTextInput from "./AppTextInput";
+import { UpdateProyectoModalProps, formCreateProyect } from "../types";
+import { gql, useMutation } from "@apollo/client";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const DELETE_EQUIPO = gql`
+  mutation removeEquipo($input: findEquipoByIdDto!) {
+    removeEquipo(findEquipoByIdInput: $input) {
+      id
+    }
+  }
+`;
+
+const UPDATE_EQUIPO = gql`
+  mutation updateEquipo(
+    $input: updateEquipoDto!
+    $inputId: findEquipoByIdDto!
+  ) {
+    updateEquipo(updateEquipoInput: $input, findEquipoByIdInput: $inputId) {
+      nombre
+    }
+  }
+`;
+
+const schema = yup.object().shape({
+  nombre: yup.string().required("Campo requerido"),
+  area: yup.string().required("Campo requerido"),
+});
+
+const ModalTarea: React.FC<UpdateProyectoModalProps> = ({
+  visible,
+  hideModal,
+  nombre,
+  idEquipo,
+}) => {
+  const [deleteEquipo, { loading: loadingDelete, error: errorDelete }] =
+    useMutation(DELETE_EQUIPO);
+  const [updateEquipo, { loading: loadingUpdate, error: errorUpdate }] =
+    useMutation(UPDATE_EQUIPO);
+  const containerStyle = { backgroundColor: "white", padding: 20 };
+
+  const navigation = useNavigation();
+
+  const funcDeleteEquipo = () => {
+    const findEquipoByIdInput = {
+      id: idEquipo,
+    };
+    deleteEquipo({
+      variables: {
+        input: findEquipoByIdInput,
+      },
+    })
+      .then(() => {
+        hideModal();
+      })
+      .catch((error) => {
+        console.error("Error al eliminar el equipo", error);
+      });
+  };
+  const funcActualizarEquipo: SubmitHandler<formCreateProyect> = (formData) => {
+    const findEquipoByIdInput = {
+      id: idEquipo,
+    };
+    const updateEquipoInput = {
+      nombre: formData.nombre,
+    };
+    updateEquipo({
+      variables: {
+        input: updateEquipoInput,
+        inputId: findEquipoByIdInput,
+      },
+    })
+      .then(() => {
+        console.log("Equipo Actualizado con exito", formData.nombre);
+        hideModal();
+      })
+      .catch((error) => {
+        console.error("Error al Actualizar el equipo", error);
+      });
+  };
+
+  const {
+    reset,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      nombre: "",
+      area: "",
+    },
+  });
+
+  return (
+    <Portal>
+      <Modal
+        visible={visible}
+        onDismiss={hideModal}
+        contentContainerStyle={containerStyle}
+      >
+        <Text
+          style={{
+            fontFamily: Font["poppins-semiBold"],
+            fontSize: FontSize.large,
+            textAlign: "center",
+            marginBottom: 10,
+          }}
+        >
+          {nombre}
+        </Text>
+
+        <View style={{ width: "100%", marginBottom: 10 }}>
+          <Divider />
+        </View>
+
+        <View>
+          <Text
+            style={{
+              fontFamily: Font["poppins-semiBold"],
+            }}
+          >
+            Nombre
+          </Text>
+        </View>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, value } }) => (
+            <AppTextInput
+              value={value}
+              onChangeText={onChange}
+              placeholder={nombre}
+            />
+          )}
+          name="nombre"
+        />
+
+        <View
+          style={{
+            marginVertical: 20,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            mode="outlined"
+            onPress={handleSubmit(funcActualizarEquipo)}
+            style={{ width: "45%", marginRight: 10 }}
+          >
+            Actualizar
+          </Button>
+          <Button
+            mode="outlined"
+            onPress={funcDeleteEquipo}
+            style={{ width: "45%", marginLeft: 10 }}
+          >
+            Eliminar
+          </Button>
+        </View>
+
+        <View style={{ width: "100%", marginTop: 15, marginBottom: 20 }}>
+          <Divider />
+        </View>
+      </Modal>
+    </Portal>
+  );
+};
+
+export default ModalTarea;
--- screens/Tareas/TareaEditScreen.tsx
+++ screens/Tareas/TareaEditScreen.tsx
@@ -1,247 +1,240 @@
 import {
-    Dimensions,
-    View,
-    Text,
-    ScrollView,
-    TouchableOpacity,
-  } from "react-native";
-  import Spacing from "../../constants/Spacing";
-  import Font from "../../constants/Font";
-  import FontSize from "../../constants/FontSize";
-  import * as yup from "yup";
+  Dimensions,
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import Spacing from "../../constants/Spacing";
+import Font from "../../constants/Font";
+import FontSize from "../../constants/FontSize";
+import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-  import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
-  import { SubmitHandler, useForm, Controller } from "react-hook-form";
-  import { useNavigation } from "@react-navigation/native";
+import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
+import { SubmitHandler, useForm, Controller } from "react-hook-form";
+import { useNavigation } from "@react-navigation/native";
 import { formCreateIntegrante } from "../../types";
 import { Schema } from "yup";
 import AppTextInput from "../../components/AppTextInput";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Colors from "../../constants/Colors";
 import { ActivityIndicator } from "react-native-paper";
-  
-  const { height } = Dimensions.get("window");
+
+const { height } = Dimensions.get("window");
 
 const EDITAR_TAREA = gql`
-mutation updateTarea($input: updateTareaDto!, $inputId: findTareaDto!) {
-  updateTarea(updateTareaInput: $input, findTareaByIdInput: $inputId) {
-    descripcion,
-    estado
+  mutation updateTarea($input: updateTareaDto!, $inputId: findTareaDto!) {
+    updateTarea(updateTareaInput: $input, findTareaByIdInput: $inputId) {
+      descripcion
+      estado
+    }
   }
-}
-`
+`;
 
-  
-  export default function TareaEditScreen({ route }) {
-    const {
-      idProyecto,
-      nombreProyecto,
-      idEquipo,
-      nombreEquipo,
-      idTarea,
-      nombreTarea,
-      estadoTarea,
-    } = route.params;
-    const navigation = useNavigation();
-    const [editTarea, { loading: cargando, error: errores }] = useMutation(EDITAR_TAREA);
-    const onCursoPressSend = () => {
-      const findTareaByIdInput = {
-        id: idTarea
-      }
-      const updateTareaInput = {
-          estado: "En Curso",
-          descripcion: nombreTarea,
-      };
-      editTarea({
-          variables: {
-              input: updateTareaInput,
-              inputId: findTareaByIdInput
-          },
-      });
-
-      navigation.navigate("Tareas");
+export default function TareaEditScreen({ route }) {
+  const {
+    idProyecto,
+    nombreProyecto,
+    idEquipo,
+    nombreEquipo,
+    idTarea,
+    nombreTarea,
+    estadoTarea,
+  } = route.params;
+  const navigation = useNavigation();
+  const [editTarea, { loading: cargando, error: errores }] =
+    useMutation(EDITAR_TAREA);
+  const onCursoPressSend = () => {
+    const findTareaByIdInput = {
+      id: idTarea,
     };
-
-    const onCompletadaPressSend = () => {
-      const findTareaByIdInput = {
-        id: idTarea
-      }
-      const updateTareaInput = {
-          estado: "Completada",
-          descripcion: nombreTarea,
-      };
-      editTarea({
-          variables: {
-              input: updateTareaInput,
-              inputId: findTareaByIdInput
-          },
-      });
-
-      navigation.navigate("Tareas");
+    const updateTareaInput = {
+      estado: "En Curso",
+      descripcion: nombreTarea,
     };
+    editTarea({
+      variables: {
+        input: updateTareaInput,
+        inputId: findTareaByIdInput,
+      },
+    });
 
-    const onEliminadaPressSend = () => {
-      const findTareaByIdInput = {
-        id: idTarea
-      }
-      const updateTareaInput = {
-          estado: "Eliminada",
-          descripcion: nombreTarea,
-      };
-      editTarea({
-          variables: {
-              input: updateTareaInput,
-              inputId: findTareaByIdInput
-          },
-      });
+    navigation.navigate("Tareas");
+  };
 
-      navigation.navigate("Tareas");
+  const onCompletadaPressSend = () => {
+    const findTareaByIdInput = {
+      id: idTarea,
     };
-      
-    return (
-        <SafeAreaView>
+    const updateTareaInput = {
+      estado: "Completada",
+      descripcion: nombreTarea,
+    };
+    editTarea({
+      variables: {
+        input: updateTareaInput,
+        inputId: findTareaByIdInput,
+      },
+    });
+
+    navigation.navigate("Tareas");
+  };
+
+  const onEliminadaPressSend = () => {
+    const findTareaByIdInput = {
+      id: idTarea,
+    };
+    const updateTareaInput = {
+      estado: "Eliminada",
+      descripcion: nombreTarea,
+    };
+    editTarea({
+      variables: {
+        input: updateTareaInput,
+        inputId: findTareaByIdInput,
+      },
+    });
+
+    navigation.navigate("Tareas");
+  };
+
+  return (
+    <SafeAreaView>
+      <View
+        style={{
+          padding: Spacing * 2,
+          marginTop: 30,
+        }}
+      >
         <View
           style={{
-            padding: Spacing * 2,
-            marginTop: 30,
+            alignItems: "center",
+            paddingTop: 20,
           }}
         >
-          <View
+          <Text
             style={{
-              alignItems: "center",
+              fontFamily: Font["poppins-semiBold"],
+              fontSize: 30,
+              maxWidth: "60%",
+              textAlign: "center",
+            }}
+          >
+            Tarea: {nombreTarea}
+          </Text>
+        </View>
+
+        <View
+          style={{
+            alignItems: "center",
+            paddingTop: 20,
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: Font["poppins-semiBold"],
+              fontSize: 30,
+              maxWidth: "60%",
+              textAlign: "center",
+            }}
+          >
+            Estado: {estadoTarea}
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            marginVertical: 20,
+            backgroundColor: "#005050",
+            height: 80,
+            borderRadius: 10,
+          }}
+          onPress={onCursoPressSend}
+        >
+          <Text
+            style={{
+              fontSize: 25,
+              width: 350,
+              color: "white",
+              textAlign: "center",
               paddingTop: 20,
             }}
           >
-            <Text
-              style={{
-                fontFamily: Font["poppins-semiBold"],
-                fontSize: 30,
-                maxWidth: "60%",
-                textAlign: "center",
-              }}
-            >
-              Tarea: {nombreTarea}             
-            </Text>
-          </View>
-        
-          <View
+            En curso
+          </Text>
+          <Text
             style={{
-              alignItems: "center",
+              width: 350,
+              color: "red",
+              textAlign: "center",
+            }}
+          ></Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            marginVertical: 20,
+            backgroundColor: "blue",
+            height: 80,
+            borderRadius: 10,
+          }}
+          onPress={onCompletadaPressSend}
+        >
+          <Text
+            style={{
+              fontSize: 25,
+              width: 350,
+              color: "white",
+              textAlign: "center",
               paddingTop: 20,
             }}
           >
-            <Text
-              style={{
-                fontFamily: Font["poppins-semiBold"],
-                fontSize: 30,
-                maxWidth: "60%",
-                textAlign: "center",
-              }}
-            >
-              Estado: {estadoTarea}             
-            </Text>
-          </View>
-
-          <TouchableOpacity
+            Compleatada
+          </Text>
+          <Text
             style={{
-                marginVertical: 20,
-                backgroundColor: "#005050",
-                height: 80,
-                borderRadius: 10,
+              width: 350,
+              color: "red",
+              textAlign: "center",
             }}
-            onPress={onCursoPressSend}
-            >
-            <Text
-                style={{
-                fontSize: 25,
-                width: 350,
-                color: "white",
-                textAlign: "center",
-                paddingTop: 20,
-                }}
-            >
-                En curso
-            </Text>
-            <Text
-                style={{
-                width: 350,
-                color: "red",
-                textAlign: "center",
-                }}
-            >
-            </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
+          ></Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            marginVertical: 20,
+            backgroundColor: "red",
+            height: 80,
+            borderRadius: 10,
+          }}
+          onPress={onEliminadaPressSend}
+        >
+          <Text
             style={{
-                
-                marginVertical: 20,
-                backgroundColor: "blue",
-                height: 80,
-                borderRadius: 10,
+              fontSize: 25,
+              width: 350,
+              color: "white",
+              textAlign: "center",
+              paddingTop: 20,
             }}
-            onPress={onCompletadaPressSend}
-            >
-            <Text
-                style={{
-                    fontSize: 25,
-                width: 350,
-                color: "white",
-                textAlign: "center",
-                paddingTop: 20,
-                }}
-            >
-                Compleatada
-            </Text>
-            <Text
-                style={{
-                width: 350,
-                color: "red",
-                textAlign: "center",
-                }}
-            >
-            </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
+          >
+            Eliminar
+          </Text>
+          <Text
             style={{
-                marginVertical: 20,
-                backgroundColor: "red",
-                height: 80,
-                borderRadius: 10,
+              width: 350,
+              color: "red",
+              textAlign: "center",
             }}
-            onPress={onEliminadaPressSend}
-            >
-            <Text
-                style={{
-                    fontSize: 25,
-                width: 350,
-                color: "white",
-                textAlign: "center",
-                paddingTop: 20,
-                }}
-            >
-                Eliminar
-            </Text>
-            <Text
-                style={{
-                width: 350,
-                color: "red",
-                textAlign: "center",
-                }}
-            >
-            </Text>
-            </TouchableOpacity>
+          ></Text>
+        </TouchableOpacity>
 
-
-  
-          <View style={{ height: 40, paddingTop: 20 }}>
-            {/* {error && (
+        <View style={{ height: 40, paddingTop: 20 }}>
+          {/* {error && (
               <Text style={{ color: "red", textAlign: "center" }}>
                 {error.message}
               </Text>
             )} */}
-          </View>
         </View>
-      </SafeAreaView>
-    );
-  }
-  
+      </View>
+    </SafeAreaView>
+  );
+}
